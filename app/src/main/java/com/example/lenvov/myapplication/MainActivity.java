2f18e5e85d1783f6a568f2f479d1117944ac6e51
LGZ_lenvov <2084864152@qq.com>
2017-12-26 10:01:51 +0800

    add textview

---
+++ app/src/main/java/com/example/lenvov/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lenvov.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
